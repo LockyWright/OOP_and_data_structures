331775db21ddf6eedfeee115a633dfc33f0fddeb
LockyWright <lockywright4@gmail.com>
2026-03-17 17:57:54 +0000

    added 2 car objects with test case

--- car.py
+++ car.py
@@ -8,3 +8,12 @@ class Car:
 
     def brake(self):
         self.speed -= 10
+
+car1 = Car("red", 0)
+car2 = Car("blue", 30)
+
+car1.accelerate()
+print(car1.speed) #should print 10
+
+car2.brake()
+print(car2.speed) #should print 20
