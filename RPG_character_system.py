acdb902e2643809f257aca25a3c6e5282fdb5a85
LockyWright <lockywright4@gmail.com>
2026-03-23 20:05:38 +0000

    deleted the whole file and re-wrote from scratch to test how much of the syntax and learnings had truly gone in. Got a little stuck not using () on a couple methods, and also with using super(), but these were minor hiccups

--- RPG_character_system.py
+++ RPG_character_system.py
@@ -3,50 +3,54 @@ class Character:
         self.name = name
         self.__health = health
         self.attack_power = attack_power
-
+    
     def get_health(self):
         return self.__health
-    
+
     def set_health(self, new_health):
         self.__health = new_health
-
-    def health_check(self):
-        if self.__health <= 0:
-            print(self.name, "has been KO'd")
-        else:
-            print(self.name, "has", self.__health, "health remaining")
-        
+    
     def attack(self, target):
         target.set_health(target.get_health() - self.attack_power)
     
-class Warrior(Character):
-    def __init__(self, name):
-        super().__init__(name, health=50, attack_power=10)
-        self.weapon = "Club"
-    def describe(self):
-        print(self.name, "has", self.get_health(), "health, and uses a", self.weapon, "to inflict", self.attack_power, "attack power")
-
+    def check_health(self):
+        if self.__health <= 0:
+            print(self.name, "has been KO'd!")
+        else:
+            print(self.name, "has", self.__health, "health remaining")
+    
 class Mage(Character):
     def __init__(self, name):
         super().__init__(name, health=40, attack_power=20)
-        self.weapon = "Staff"
+        self.weapon = "staff"
     def describe(self):
-        print(self.name, "has", self.get_health(), "health, and uses a", self.weapon, "to inflict", self.attack_power, "attack power")
+        print("A Mage named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power.", self.name, "wields a", self.weapon, ".")
 
-Warrior1 = Warrior("Brute")
-Mage1 = Mage("Wizard")
+class Warrior(Character):
+    def __init__(self, name):
+        super().__init__(name, health=50, attack_power=10)
+        self.weapon = "club"
+    def describe(self):
+        print("A Warrior named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power.", self.name, "wields a", self.weapon, ".")
 
-Warrior1.describe()
+Mage1 = Mage("Gandalf")
+Warrior1 = Warrior("Heavy")
+
 Mage1.describe()
+Warrior1.describe()
 
+
+Mage1.attack(Warrior1)
 Warrior1.attack(Mage1)
-Mage1.health_check()
+Mage1.check_health()
+Warrior1.check_health()
 
 Mage1.attack(Warrior1)
-Warrior1.health_check()
+Warrior1.attack(Mage1)
+Mage1.check_health()
+Warrior1.check_health()
 
 Mage1.attack(Warrior1)
-Warrior1.health_check()
-
 Mage1.attack(Warrior1)
-Warrior1.health_check()
+Mage1.check_health()
+Warrior1.check_health()
