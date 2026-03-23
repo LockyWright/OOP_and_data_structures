d326bfc1bcb6688d6b6e495726bb5cd92f3193e5
LockyWright <lockywright4@gmail.com>
2026-03-23 21:07:41 +0000

    Added describe fallback in the parent class, and an additional Alien character. Tested polymorphism of the describe() method.

--- RPG_character_system.py
+++ RPG_character_system.py
@@ -18,28 +18,38 @@ class Character:
             print(self.name, "has been KO'd!")
         else:
             print(self.name, "has", self.__health, "health remaining")
+
+    def describe(self):
+        print(self.name, "has", self.get_health(), "health, and", self.attack_power, "attack power.")
     
 class Mage(Character):
     def __init__(self, name):
         super().__init__(name, health=40, attack_power=20)
-        self.weapon = "staff"
     def describe(self):
-        print("A Mage named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power.", self.name, "wields a", self.weapon, ".")
+        print("A Mage named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power. Mage's are magic and powerful.")
 
 class Warrior(Character):
     def __init__(self, name):
         super().__init__(name, health=50, attack_power=10)
-        self.weapon = "club"
     def describe(self):
-        print("A Warrior named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power.", self.name, "wields a", self.weapon, ".")
+        print("A Warrior named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power. Warrior's are slow to fight.")
 
+class Alien(Character):
+    def __init__(self, name):
+        super().__init__(name, health=30, attack_power=30)
+    def describe(self):
+        print("An Alien named", self.name, "with", self.get_health(), "health, and", self.attack_power, "attack power. Alien's are from another planet.")
+
+#testing polymorphism
 Mage1 = Mage("Gandalf")
 Warrior1 = Warrior("Heavy")
+Alien1 = Alien("Martian")
 
-Mage1.describe()
-Warrior1.describe()
+characters = [Mage("Gandalf"), Warrior("Heavy"), Alien("Martian")]
+for character in characters:
+    character.describe()
 
-
+#attack system
 Mage1.attack(Warrior1)
 Warrior1.attack(Mage1)
 Mage1.check_health()
